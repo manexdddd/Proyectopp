06776fead00846fe361a80ec53d9aba455d48d9a
Antonio <antonio.jose.medina.e@gmail.com>
2023-10-15 13:02:10 -0600

    asdf

--- src/app/home-public/home-public.component.ts
+++ src/app/home-public/home-public.component.ts
@@ -10,8 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-public.component.css']
 })
 export class HomePublicComponent  {
+  counter = 0;
+  text = ['CEO DevFolio,Frontend Developer,Graphic Designer', 'Web Developer', ',Web Designer3'];
   constructor() {}
-  
-  
- 
+  ngOnInit(): void {
+    setInterval(() => this.change(), 3000);
+  }
+
+  change() {
+    let elem = document.querySelector(".text-slider-items");
+    if (elem) {
+      elem.innerHTML = this.text[this.counter];
+      this.counter++;
+      if (this.counter >= this.text.length) { this.counter = 0; }
+      let elems = document.querySelectorAll<HTMLElement>(".text-slider-items");
+
+  elems.forEach((elem) => {
+    elem.style.animation = 'none';
+    elem.offsetHeight; /* trigger reflow */
+    elem.style.animation = ''; 
+  })
 }
+
+      
+}
+}
+
