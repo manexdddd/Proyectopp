e81b9e4743e8a4865dadd11d0e99392046640f28
manexdddd <manevergara14@gmail.com>
2023-11-05 15:50:10 -0600

    auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
 import { GoogleAuthProvider } from '@angular/fire/auth';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { BehaviorSubject, finalize } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -15,10 +16,10 @@ export class AuthService {
   role:any
   private progreso = new BehaviorSubject<boolean>(false);
     currentprogreso = this.progreso.asObservable();
-    constructor(private afauth:AngularFireAuth,private router: Router,private  storage: AngularFireStorage) {
+    constructor(private afauth:AngularFireAuth,private afs: AngularFirestore,private router: Router,private  storage: AngularFireStorage,
+      ) {
       
      }
-
      changeprogreso(value: boolean) {
       this.progreso.next(value);
     }
@@ -59,11 +60,37 @@ async registro(email:string,psw :string){
 
     
 
-   
+crearDocumento(alumnoid: string, data: any) {
+  //referencia del documento 
+  const documentRef = this.afs.collection('alumnos').doc(alumnoid);
 
 
+  //ingresa los datos al documento
+  return documentRef.set(data)
+    .then(() => {
+      console.log('Documento creado con éxito');
+      this.router.navigate(['/login']);
+    })
+    .catch(error => {
+      console.error('Error al crear el documento:', error);
+    });
+
+}
+
+//cerrar sesion
+async logout() {
+  localStorage.setItem('email', "");
+  localStorage.setItem('rolx', "");
+    await this.afauth.signOut();
+    
+    this.router.navigate(['/login']);
+ 
+
+}
     
 
-
+getusuario() {
+  return this.afauth.authState;
+}
 
 }
