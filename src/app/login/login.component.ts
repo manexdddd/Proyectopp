05e0c64348301c170245d05cbd05a60c5ed8bc41
Antonio <antonio.jose.medina.e@gmail.com>
2023-11-07 02:22:38 -0600

    asdf

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+/*
+  registroForm! : FormGroup;
 
+  constructor(private  fb: FormBuilder) {}
+
+  ngOnInit(): void{
+    this.registroForm = this.initForm();
+  }
+
+  onSubmit(): void {
+    console.log('Form ->', this.registroForm.value);
+  }
+  initForm(): FormGroup{
+    return this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      surnameM: ['', [Validators.required]],
+      surnameP: ['', [Validators.required]],
+      numberC: ['', [Validators.required]],
+      career: ['', [Validators.required]],
+    },{validators: this.confirmPasswordValidator});
+  }
+  confirmPasswordValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+    return password && confirmPassword && password.value == confirmPassword.value ? null : { mismatch: true };
+  }
+  //Funcion para convertir los datos del email a minusculas
+  lowecase(){
+    const emailControl = this.registroForm.get('email');
+    emailControl?.setValue(emailControl.value.toLowerCase(), {emitEvent: false});
+  }*/
 }
