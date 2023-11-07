bfc7fe8f0d523931121d0a86b10f8310fd9b39ea
manexdddd <manevergara14@gmail.com>
2023-11-06 19:13:54 -0600

    admin

--- src/app/sidebar-admin/sidebar-admin.component.ts
+++ src/app/sidebar-admin/sidebar-admin.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { AlumnosService } from '../services/alumnos.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-sidebar-admin',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar-admin.component.css']
 })
 export class SidebarAdminComponent {
+  currentUsuario: any;
+  usuario: any;
+  email: string;
+  constructor(private renderer: Renderer2, private elementRef: ElementRef, private alu:AlumnosService,
+    private auth:AuthService) {}
+  ngOnInit(): void {
+     
+    this.email = localStorage.getItem('email');
+
+    this.alu.getalumno(this.email).subscribe(usuario => {
+      this.usuario = usuario;
+    });
+  }
+
+  cerrar(){
+    localStorage.setItem('email', "");
+    localStorage.setItem('rolx', "");
+    this.auth.logout();
+  }
 
 }
